1174857e4a0f4a56e8a44464d5d65e2acc9532fe
akhilbabu759 <akhilb4001@gmail.com>
2022-11-07 00:39:03 +0530

    intro screen addad but error ,set state must change in submit the name

--- lib/pages/splash-screen.dart
+++ lib/pages/splash-screen.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:moneypouch/pages/home.dart';
+import 'package:moneypouch/pages/intro.dart';
+import 'package:moneypouch/pages/one%20time/welcome.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -10,17 +14,64 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+   void goHom(){
+     Navigator.of(context).pushReplacement(
+           MaterialPageRoute(builder: (context) =>  const Home()));
+
+      
+    }
+   void goIntro(){
+     Navigator.of(context).pushReplacement(
+           MaterialPageRoute(builder: (context) =>  const Welcome()));
+
+
+   }
+
+  
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        ()  async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool seen = (prefs.getBool('seen') ?? false);
+
+    if (seen) {
+       goHom();
+    } else {
+      await prefs.setBool('seen', true);
+      goIntro();
+      
+     
+    }
+  
+        //  Navigator.pushReplacement(
+           }   //     context, MaterialPageRoute(builder: (context) =>  IntroductionScreens())));
+    );
+   
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(136, 20, 27, 38),
       body: SafeArea(
           child: Center(
-        child: Column(mainAxisSize: MainAxisSize.max,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
-          children:const [
-            Icon(Icons.money, color: Colors.deepOrangeAccent),
-            Text(
+          children: const[
+           CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage(
+                  'assets/images/mammon.jpg',
+                )),
+            SizedBox(
+              height: 1,
+            ),
+             Text(
               'MONEY POUCH',
               style: TextStyle(
                   fontSize: 20,
@@ -32,7 +83,8 @@ class _SplashState extends State<Splash> {
             ),
             SizedBox(
                 width: 200,
-                child: ClipRRect( borderRadius: BorderRadius.all(Radius.circular(40)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
                   child: LinearProgressIndicator(
                     color: Color.fromARGB(255, 7, 255, 255),
                     backgroundColor: Colors.white,
@@ -45,3 +97,6 @@ class _SplashState extends State<Splash> {
     );
   }
 }
+
+
+
